8c6618cd7ca0e7527964b5b7b490156bd181187f
tom-englert <mail@tom-englert.de>
2020-10-29 13:40:37 +0100

    V 1.4

--- ProjectMigrationHelper/Properties/AssemblyInfo.cs
+++ ProjectMigrationHelper/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ namespace ProjectMigrationHelper.Properties
 {
     static class Product 
     {
-        public const string Version = "1.3.0.0";
+        public const string Version = "1.4.0.0";
     }
 }
